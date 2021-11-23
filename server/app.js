fe94936fd38bc234a93cc03ca3698e58bd9d2861
Kunal-2001 <kunal999936@gmail.com>
2021-11-23 19:26:31 +0530

    Folder restructure

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("./config/database");
+const indexRoute = require("./routes/index");
+// set up express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// set up routes
+
+app.use("/", indexRoute);
